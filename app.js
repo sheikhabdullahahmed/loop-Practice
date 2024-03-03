0eb8b3983fc69810b61619916b2ef95ece32ca3f
sheikhabdullahahmed <sheikhabdullahahmed>
2024-03-04 01:08:39 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,620 @@
+// for(let i=2; i<=100; i++) {
+//     console.log (i);
+
+// }
+
+// let sum= 0;
+// for ( let i= 1; i <=5; i++) {
+//     sum = sum + i; 
+// }
+
+// let  sum = 10; 
+// for (let i =1; i <= 5; i++) [
+//     console.log (sum)
+// ]
+
+// console.log("sum = ", "sum");
+
+// for (let i =1; i <= 5; i++) {
+//     console.log ("i = " , i);
+// }
+
+// for (let i=1; i<=40; i++) {
+//     console.log ("i =" , i)
+// }
+
+
+// let i =1;
+// while (i <=10) {
+//     document.write ("i ");
+//     i++;
+// }
+
+
+// let sum = 10;
+// for (let i= 1; i <=5; i++) {
+//     console.log ("sum")
+// }
+
+// let num =20; 
+// for ( let i=10; i<=15; i++){
+//     console.log ("num")
+// }
+
+// let sum = 50;
+// for (let i= 30; i<=10; i++) {
+//     console.log ("sum")
+// }
+
+// let num = 20;
+// for (let i=10; i<=15; i++) {
+// console.log ("num")
+// }
+
+// let game =20;
+// for (let i=10; i<=15; i++) {
+//     console.log (
+//         "game"
+//     )
+// }
+
+// let num =20; 
+// while (num <=30) {
+//     console.log (num + "<br>")
+//     num++
+// }
+
+// let game =20;
+// while (game <=30) {
+//     console.log (game + "<br>")
+//     game++
+// }
+
+// let game =50;
+// for (let i=30; i<=40; i++) {
+//     console.log (
+//         game + "<br>"
+//     )
+// }
+
+// let game = 500; 
+// for (let i= 10; i <=20; i++ ) {
+//     console.log (game)
+// }
+
+// let home = 20;
+// for (let i=10; i<=15; i++) {
+//     console.log (
+//         home + "<br>"
+//     )
+// }
+
+
+// let game = 20;
+// while (game <=30) {
+//     console.log (game +"<br>")
+//     game++
+// }
+
+
+// let home = 30;
+// for (let i= 20; i<=25; i++) {
+//     console.log (
+//         home + "<br>"
+//     )
+// }
+
+
+// let home =20;
+// while (home <=30) {
+//     console.log (home + "<br>")
+//     home++
+// }
+
+
+// let game =50;
+// while (game <=40) {
+//     console.log (game + "<br>")
+//     game++
+// }
+
+// let game =20; 
+// while (game <=30) {
+//     console.log (game)
+//     game++
+// }
+
+// let hum =30;
+// for (let i=20; i<=10; i++ ) {
+//     console.log (
+//         "hum"
+//     )
+// }
+
+// let hum =30; 
+// for (let i = 20; i<=10; i++ ) {
+//     console.log ("hum")
+// } 
+
+// let game = 20; 
+// while (game <= 30) {
+//     console.log (game) 
+//     game++
+// }
+
+// let game = 20; 
+// while (game <= 30) {
+//     console.log (game)
+//     game ++
+// }
+
+
+// let hum = 50;
+// for (let i = 30; i<=40; i++) {
+//     console.log ("apna collage")
+// }
+
+// for (let i = 0; i <= 10; i++ ) {
+//     console.log ("gamer")
+// }
+
+// let game = 30;
+// while (game <= 40) {
+//     console.log (game)
+//     game++
+// }
+
+// let game =30;
+// for (let i= 50; i<=70; i++) {
+//     console.log (game)
+// }
+
+// let game =100;
+// for ( let i = 100; i<120; i++) {
+//     console.log (game)
+// }
+
+// let game = 30;
+// while (game <= 40) {
+//     console.log (game)
+//     game++
+// }
+
+// let hum = 40;
+// while (hum<=50) {
+//     console.log (hum )
+//     hum++
+// }
+
+// let hum = 40; 
+// while (hum <= 50) {
+//     console.log (hum)
+//      hum++
+// }
+// let hum = 40;
+// while (hum <= 50) {
+//     console.log (hum)
+//     hum++
+// }
+
+// let game = "Vice City";
+// let result;
+// for (let i = 0; i <= 10; i++) {
+//     console.log ('a');
+// }
+
+// let hum = 40;
+// while (hum <= 50) {
+//     console.log ( typeof "kkkk")
+//     hum++
+// }
+
+// let hum = 30;
+// while (hum <= 40) {
+//     console.log ("Gamer")
+//     hum++
+// }
+
+// let hum = 30; 
+// while (hum <= 40) {
+//     console.log ("Gamer")
+//     console.log ("hh")
+//     console.log ("kkkkk")
+//     hum++
+// }
+
+
+// let gameNum = 25; 
+// let userNum = prompt ("Gussse The Number ");
+
+// while (userNum != gameNum) {
+//     userNum = prompt ("You Wroung number, Gusse Again")
+// }
+
+// console.log ("Congulaton ,You Are Win");
+
+// let game = 25;
+// let userNum = prompt ('Enteer Your Value')
+
+// while(userNum != game) {
+//     userNum = prompt ("Gusse Your Are Wrong")
+// }
+
+// console.log ("youare Win")
+
+// let gameNum = 25;
+// let userNum = prompt ("Gusse The Numebr");
+
+// while (userNum != gameNum ) {
+//     userNum = prompt ("you Wrong number, Gusse Again")
+// }
+// alert ("Ypu ")
+
+// let gameNum = 25; 
+// let userNum = prompt ("You Wrong Number");
+// while (userNum != gameNum) {
+//     userNum = prompt ("hhhh")
+// }
+// alert("jjjj")
+
+
+
+// let game = 20;
+// let userNum = prompt ('Enter You  Value')
+
+// while (userNum != game) {
+//     userNum = prompt ("Gusses Youur Are Wrong")
+// }
+
+// console.log ("your Aer Win");
+
+// let arr = ["Ali", "Sheikh", "Abdul", "Abdullah"];
+// let arr2 = ["Expertizo", "Loin", "Samed"];
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0 ; j < arr2.length ; j++) {
+//         console.log (arr [i] +"" + arr2 [j] + "<br>")
+//     }
+// }
+
+// let arr = ["Abdul", "Ahmed", "Ali", "Moosa", "Abbdul"];
+// let arr2 = ["Expertizo", "Sameed"];
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0 ; j < arr2.length ; j++) {
+//         console.log (arr[i] + "" + arr2 [j]);
+//     }
+// }
+
+// let arr = ["Sheikh", "Abdullah", "Ahmed", "Sameed", "Rafay"];
+// let arr2 = ["Experitzo", "Don"];
+// for (let i =0 ; i <arr.length; i++) {
+//     for (let j =0; j < arr2.length ; j++) {
+//         console.log (arr[i] + "" + arr2 [j]);
+//     }
+// }
+
+
+// let arr = ["Sheikh", "Abdullah", "abdul", "Smeed"];
+// let arr2 = ["Expertizo", "Sameed"];
+// for (let i =0; i <arr.length ; i++) {
+//     for (let j= 0; j <arr2.length; j++) {
+//         console.log (arr[i] + "" + arr2 [j])
+//     }
+// }
+
+// let arr = ["Abdullah", "Sheikh Abdullah", "Abdullah"];
+// let arr2 = ["Home", "Abdul", "Sheilh"];
+
+// for (let i = 0; i<=arr.length; i++) {
+//     for (let j =i ; j <= arr2.length ; j++) {
+//         document.write (arr[i] + "" + arr2 [j])
+//         document.write ("<br>")
+//     }
+// }
+
+
+
+// let arr = ["Abdullah", "Ali", "Abdul", "Ahmed", ];
+// let arr2 = ["Sameed", "House"];
+// for (let i = 0; i < arr .length ; i++) {
+//     for (let j = 0; j < arr2 .length ; j++) {
+//         console.log (arr [i] + "" + arr2 [j] )
+//     }
+// }  
+
+
+// let arr = ["aali", "Abdul", "Abdul", "Ahmed" ];
+// let arr2 = ["Ahmesd", "Sun", "Moon"]
+
+// for (let i = 0; i < arr.length ; i++) {
+//     for (let j =0; j < arr2 . length ; j++) {
+//         console.log (arr [i] + "" + arr2 [j]); 
+//     }
+// }
+
+// let arr = ["Sheikh ", "Abdullah", "Sheikh", "Abdul", "Sameed", "Gmaer"];
+// let arr2 = ["Expertizo", "Gulab"];
+// for (let i = 0; i <arr.length; i++) {
+//     for (let j = 0; j < arr2 .length; j++) {
+//         document.write (arr [i] + ""  + arr2 [j])
+//     }
+// }
+
+
+
+
+
+
+
+// let arr = ["Ali", "Abdul", "Ahmed", "SUN", "Hmaza"];
+// let arr2 = ["HAMZA", "ABDULLAH"];
+// for (let i = 0; i < arr.length; i ++) {
+//     for (let j = 0; j < arr2.length ; j++) {
+//         console.log (arr[i] + "" + arr2[j])
+//     }
+// }
+
+// for (let i = 1; i <=100 ; i++) {
+//     for (let j = i ; j <= i; j++) {
+//         console.log (j +'')
+//     }
+//     console.log (j)
+// }
+
+// for (let i = 1; i <= 100; i +=  10) {
+//     for (let j = i ; j < i + 10 && j  <= 100;  j++ ) {
+//         document.write(j +  "" );
+//     }
+
+//     document.write("<br>");
+// }
+
+// for (let i = 1; i<=100; i+= 10) {
+//     for (let j =i; j < i + 10 && j<= 100; j++) {
+//         document.write (j + "");
+//     }
+//     document.write("<br>")
+// }
+
+// for (let i =1; i <= 100; i+= 10 ) {
+//     for (let j = i; j < i + 10 && j <=100; j++) {
+//         document.write (j +'')
+//     }
+//     document.write("<br>")
+// }
+
+// let arr = ["Ali", "ABDUL", "SHEIKH", "ABDULLAH"];
+// let arr2 = ["Abdul", "Car"];
+// for (let i =0; i < arr.length ; i++) {
+//     for (let j = 0 ; j < arr2 .length ; j++) {
+//         console.log (arr[i] + "" + arr2 [j])
+//     }
+// }
+
+// let arr = ["Ali ", "Sheikh ", "Abdullah", "Ahmed"];
+// let arr2 = ["House", "Gamer"]; 
+
+// for (let i =0; i < arr.length ; i++) {
+//     for (let j = 0; j < arr2.length ; j++) {
+//         document.write (arr [i] + "" + arr2 [i]);
+//     }
+// }
+
+// let arr = ["Ali", "Sheikh ", "Abdullah", "Sheikh "];
+// let arr2 = ["Experitzo", "House", "Ghar" ,"jjj"];
+// for (let i = 0; i < arr.length ; i++) {
+//     for (let j = 0; j < arr2.length ; j++) {
+        
+//     }document.write(arr[i] + "" + arr2 [i] )
+//     document.write ("<br>")
+// }
+
+
+
+// let arr = ["Ali", "Sheikh", "Abdullah", "Sheikh Abdullah"];
+// let arr2 = ["Sheikh Abdullah", "House"]
+
+// for (let i =0; i < arr.length; i++) {
+//     for(let j= 0 ; j <arr2.length ; j++) {
+//         document.write(arr[i] + '' + arr2 [i])
+//         document.write ("<br>")
+//     }
+// }
+
+// for (let i = 1; i <= 100; i+= 10) {
+//     for (let j = i; j < i + 10 && j <100; j++) {
+//         document.write(j + "")
+//     }
+//     document.write ("<br>")
+// }
+
+// for (let i = 1; i <=100; i+= 10) {
+//     for (let j = i; j < i + 10 && j < 100; j++) {
+//         document.write (j + "")
+//     }
+//     document.write ("<br>")
+// }
+
+
+// for (let i =1 ; i <= 5; i+= 5) {
+//     for (let j = i ; j < i + 5 && j < 5; j++ ) {
+//         document.write (j + "")
+//     }
+
+    
+// }
+
+
+// for (let i = 5 ; i >= 1; i--) {
+//     for (let j = 5; j >= i; j-- ) {
+//         document.write(j + "")
+//     }
+//     document.write("<br>")
+// }
+
+// for (let i =1; i <= 5; i++ ){
+//     for (let j=1; j<= i; j++) {
+//         document.write (j + "" )
+//     }
+//     document.write("<br>")
+// }
+
+// for (let i =0; i <=5; i++) {
+//     for (let j = 1; j<= i; j++ ) {
+//         document.write(j +"")
+//     }
+//     document.write ("<br>")
+// }
+
+// for (let i = 0 ; i <= 5; i++) {
+//     for (let j =1; j <=i; j++) {
+//         document.write (j+"")
+//     }
+//     document.write("<br>")
+// }
+
+// for (let i =0; i <= 15; i++) {
+//     for (let j = 10; j<=i; j++) {
+//         document.write (j + "")
+//     }
+//     document.write ("<br>")
+// }
+
+
+
+// for (let i = 5; i >= 1; i--) {
+//     for (let j = 5; j >= i ; j--) {
+//         document.write (j+ "")
+//     }
+
+//     document.write("<br>")
+// }
+// for (let i =5; i >= 1; i--) {
+//     for (let j = 5; j>= i; j--) {
+//         document.write (j + "")
+//     }
+
+//     document.write ("<br>")
+// }
+
+// for (let i =1; i<= 5; i++ ) {
+//     for (let j =1; j <= i; j++) {
+//         document.write (j + "")
+//     }
+//     document.write ("<br>")
+// } 
+
+// for (let i = 1; i<= 5; i++ ) {
+//     for (let j = 1; j <= i ; j++) {
+//         document.write (j + "")
+//     }
+//     document.write ("<br>")
+// }
+
+
+
+// for (let i =  5; i >= 1; i--) {
+//     for (let j = 5; j >= i ; j--) {
+//         document.write(j + "")
+//     }
+//     document.write("<br>")
+// }
+
+// for (let i = 5;i >= 1; i--){
+//     for (let j =5; j>= i; j-- ) [
+//         document.write (j + "")
+//     ]
+//     document.write ('<br>')
+// }
+
+
+// for (let i = 5; i >= 1; i--) {
+//     for (let j =5; j >= i ; j--) {
+//         document.write (j + "")
+//     }
+//     document.write ("<br>");
+// }
+
+
+
+
+
+// for (let i= 1; i <= 5; i++) {
+//     for (let j = 1; j <= i; j++  ) {
+//         document.write(j +"")
+//     }
+//     document.write("<br>")
+// }
+
+// for (let num =0; num <=100; num ++) {
+//     console.log (num)
+// }
+
+
+// for (let num = 0; num <=100; num++) {
+// if (num%2===0)
+
+// console.log (num)
+// }
+
+// let obj = {
+//     item:"pen",
+//     price: 10,
+
+// };
+
+
+// console.log ("the cost of", obj.item, "is" , obj.price, "rupess" );
+
+// let output = 'the cost of $ {obj.item }'
+
+// for (var i = 5; i >= 1; i--) {
+//    for (var j = 5; j >= i; j--) {
+//        document.write(j + " ");
+//    }
+//    document.write("<br>");
+// }
+
+// for (let i = 5; i >= 1; i--) {
+//     for (let j = 5; j >= i; j--) {
+//         console.log(j + "")
+//     }
+//     console.log ("<br>")
+// }
+
+
+// for (var i = 1; i <= 5; i++) {
+//    for (var j = 1; j <= i; j++) {
+//        document.write(i + " ");
+//    }
+//    document.write("<br>");
+// }
+
+
+// for (let i =1; i <= 5 ; i++ ) {
+//     for (let j = 1; j <= i; j++) {
+//         document.write (i + "")
+//     }
+//     document.write ("<br>")
+// }
+ 
+
+// for(let i =1; i <= 5; i++) {
+//     for (let j =1; j <=i; j++) {
+//         document.write(i + "")
+//     }
+//     document.write ("<br>")
+// }
+
+// for (let i =1 ; i <= 5; i++) {
+//     for (let j =1; j <=i; j++) {
+//         document.write (i + "")
+//     }
+//     document.write("<br>")
+// }
+
+// for (let i =1; i <= 5; i++) {
+//     for (let j =1 ; j <= i; j++) {
+//         document.write(i + "")
+//     }
+//     document.write("<br>")
+// }
